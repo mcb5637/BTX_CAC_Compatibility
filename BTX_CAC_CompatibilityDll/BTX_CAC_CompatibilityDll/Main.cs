ed9df296c3d093d57e3f41b0285bbd4cf50fadbc
mgrbo <mgrbo@web.de>
2021-01-15 17:44:07 +0100

    version bump

--- BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/Main.cs
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/Main.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-[assembly:AssemblyVersion("0.1.3.0")]
+[assembly:AssemblyVersion("0.1.4.0")]
 
 namespace BTX_CAC_CompatibilityDll
 {
