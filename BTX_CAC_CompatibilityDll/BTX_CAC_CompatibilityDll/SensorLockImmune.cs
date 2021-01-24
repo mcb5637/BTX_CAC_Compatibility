c02390445a81b987add3b0b2fc91a666d84219ea
mgrbo <mgrbo@web.de>
2021-01-24 16:21:53 +0100

    aiproof sensorlock blocking

--- BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/SensorLockImmune.cs
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/SensorLockImmune.cs
@@ -9,21 +9,38 @@ using Harmony;
 
 namespace BTX_CAC_CompatibilityDll
 {
-    [HarmonyPatch(typeof(SelectionStateActiveProbe), "CalcPossibleTargets")]
-    class SelectionStateActiveProbe_CalcPossibleTargets
+    [HarmonyPatch(typeof(ActiveProbeSequence), "FireWave")]
+    class ActiveProbeSequence_FireWave
     {
-        public static void Postfix(ref List<ICombatant> __result)
+        public static bool Prefix(ActiveProbeSequence __instance, AbstractActor Target, ref int ___numWavesFired, ref float ___timeSinceLastWave)
         {
-            __result = __result.Where((c) => !(c is AbstractActor) || (c as AbstractActor).StatCollection.GetValue<float>("SensorLockDefense") <= 0).ToList();
+            if (Target.StatCollection.GetValue<float>("SensorLockDefense") > 0) {
+                __instance.SetCamera(CameraControl.Instance.ShowSensorLockCam(Target, 2f), __instance.MessageIndex);
+                CameraControl.Instance.ClearTargets();
+                CombatGameState Combat = Traverse.Create(__instance).Property("Combat").GetValue<CombatGameState>();
+                Combat.MessageCenter.PublishMessage(new FloatieMessage(__instance.owningActor.GUID, Target.GUID, "Sensor Lock blocked by ECM", FloatieMessage.MessageNature.Buff));
+                ___numWavesFired++;
+                ___timeSinceLastWave = 0;
+                return false;
+            }
+            return true;
         }
     }
 
-    [HarmonyPatch(typeof(SelectionStateSensorLock), "CalcPossibleTargets")]
-    class SelectionStateSensorLock_CalcPossibleTargets
+    [HarmonyPatch(typeof(SensorLockSequence), "FireWave")]
+    class SensorLockSequence_FireWave
     {
-        public static void Postfix(ref List<ICombatant> __result)
+        public static bool Prefix(SensorLockSequence __instance, ref int ___numWavesFired, ref float ___timeSinceLastWave)
         {
-            __result = __result.Where((c) => !(c is AbstractActor) || (c as AbstractActor).StatCollection.GetValue<float>("SensorLockDefense") <= 0).ToList();
+            if (__instance.Target.StatCollection.GetValue<float>("SensorLockDefense") > 0)
+            {
+                CombatGameState Combat = Traverse.Create(__instance).Property("Combat").GetValue<CombatGameState>();
+                Combat.MessageCenter.PublishMessage(new FloatieMessage(__instance.owningActor.GUID, __instance.Target.GUID, "Sensor Lock blocked by ECM", FloatieMessage.MessageNature.Buff));
+                ___numWavesFired++;
+                ___timeSinceLastWave = 0;
+                return false;
+            }
+            return true;
         }
     }
 
