30fdc83925772857ea53926bdfbfe09631c673d2
mgrbo <mgrbo@web.de>
2021-02-13 14:21:20 +0100

    fixed unwanted floaties

---
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/AdvWeaponHitInfo_ApplyHitEffects.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustAmmoCategories;
+using Harmony;
+
+namespace BTX_CAC_CompatibilityDll
+{
+    [HarmonyPatch(typeof(AdvWeaponHitInfo), "ApplyHitEffects")]
+    class AdvWeaponHitInfo_ApplyHitEffects
+    {
+        public static void Postfix(AdvWeaponHitInfo __instance)
+        {
+            foreach (AdvWeaponResolveInfo ri in __instance.resolveInfo.Values)
+            {
+                ri.floatieMessages = ri.floatieMessages.Where((s) => !s.StartsWith("Inferno VFX")).ToList();
+            }
+        }
+    }
+}
