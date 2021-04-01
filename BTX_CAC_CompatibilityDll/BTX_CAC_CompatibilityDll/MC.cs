c4934dd64a591349dfc805a14c288103b07c6b15
mgrbo <mgrbo@web.de>
2021-04-01 17:07:04 +0200

    next story mission

--- BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/MC.cs
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/MC.cs
@@ -1,4 +1,6 @@
-﻿using Harmony;
+﻿using BattleTech;
+using BattleTech.Designed;
+using Harmony;
 using MissionControl.Rules;
 using System.Reflection;
 using UnityEngine;
@@ -26,7 +28,7 @@ namespace BTX_CAC_CompatibilityDll
             string type = MissionControl.MissionControl.Instance.CurrentContract.ContractTypeValue.Name;
             if (type == "Panzyr_Attack")
                 __result = "PlayerLanceSpawner_Attack";
-            else if (type == "Smithon_Attack")
+            else if (type=="Smithon_Attack" || type=="Story_6A_TreasureTrove")
                 __result = "PlayerLanceSpawner";
         }
     }
@@ -42,11 +44,37 @@ namespace BTX_CAC_CompatibilityDll
             //    foreach (Transform chch in ch.transform)
             //    {
             //        FileLog.Log("\t" + chch.name);
+            //        foreach (Transform chchc in chch.transform)
+            //        {
+            //            FileLog.Log("\t\t" + chchc.name);
+            //        }
             //    }
             //}
             string type = MissionControl.MissionControl.Instance.CurrentContract.ContractTypeValue.Name;
             if (type == "Story_5_ServedCold")
                 __result = "01_InitialSetup/Chunk_PlayerLance";
+            else if (type == "Story_6A_TreasureTrove")
+                __result = "Gen_PlayerLance";
+        }
+    }
+
+    [HarmonyPatch(typeof(SceneUtils), "GetRandomPositionWithinBounds")]
+    class SceneUtils_GetRandomPositionWithinBounds
+    {
+        public static bool Prefix(Vector3 target, float maxDistance, ref Vector3 __result)
+        {
+
+            GameObject chunkBoundaryRect = MissionControl.MissionControl.Instance.EncounterLayerGameObject.transform.Find("Chunk_EncounterBoundary")?.gameObject;
+            if (chunkBoundaryRect == null)
+                chunkBoundaryRect = MissionControl.MissionControl.Instance.EncounterLayerGameObject.transform.Find("Gen_EncounterBoundary").gameObject;
+            GameObject boundary = chunkBoundaryRect.transform.Find("EncounterBoundaryRect").gameObject;
+            EncounterBoundaryChunkGameLogic chunkBoundary = chunkBoundaryRect.GetComponent<EncounterBoundaryChunkGameLogic>();
+            EncounterBoundaryRectGameLogic boundaryLogic = boundary.GetComponent<EncounterBoundaryRectGameLogic>();
+            Rect boundaryRec = chunkBoundary.GetEncounterBoundaryRectBounds();
+
+            Vector3 randomRecPosition = boundaryRec.GetRandomPositionFromTarget(target, maxDistance);
+            __result = randomRecPosition.GetClosestHexLerpedPointOnGrid();
+            return false;
         }
     }
 }
