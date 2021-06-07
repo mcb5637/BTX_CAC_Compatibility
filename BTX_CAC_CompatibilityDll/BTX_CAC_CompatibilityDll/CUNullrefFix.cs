6100cd72a97d4c91b8a6163207a21868a755fdbb
mgrbo <mgrbo@web.de>
2021-06-07 14:53:33 +0200

    lam icons, lamjet fixes, fixed CU Pathing_UpdateMeleePath nullref

---
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/CUNullrefFix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using BattleTech;
+using CustomUnits;
+using System.Reflection.Emit;
+
+namespace BTX_CAC_CompatibilityDll
+{
+    [HarmonyPatch(typeof(Pathing_UpdateMeleePath), "Postfix")]
+    class CUNullrefFix
+    {
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            bool found = false;
+            foreach (CodeInstruction c in instructions)
+            {
+                //if (c.opcode == OpCodes.Ldstr && "Pathing.UpdateMeleePath ".Equals(c.operand))
+                //    c.operand = "Pathing.UpdateMeleePath Modified ";
+                if (c.opcode == OpCodes.Brfalse && found)
+                {
+                    yield return new CodeInstruction(OpCodes.Pop);
+                    c.opcode = OpCodes.Br;
+                    Main.Log.Log("fixed CU update pathing nullref");
+                }
+
+                if (c.opcode == OpCodes.Call && "Void PrependStoredPath(BattleTech.Pathing, System.Collections.Generic.List`1[BattleTech.PathNode] ByRef)".Equals(c.operand.ToString()))
+                    found = true;
+
+                //FileLog.Log($"{c.opcode} {(c.operand == null ? "null" : c.operand.ToString())} {(c.operand == null ? "null" : c.operand.GetType().FullName)}");
+                yield return c;
+            }
+        }
+    }
+}
