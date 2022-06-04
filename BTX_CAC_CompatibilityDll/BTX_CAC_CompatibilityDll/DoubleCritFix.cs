e6739cee0ad2ab333be6ce0c18c21b4830e4447c
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2022-06-04 17:56:28 +0200

    fixed a atm advancedpatch syntax, possible double crit fix

---
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/DoubleCritFix.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using BattleTech;
+using System.Diagnostics;
+
+namespace BTX_CAC_CompatibilityDll
+{
+    [HarmonyPatch(typeof(MechComponent), "DamageComponent")]
+    internal class MechComponent_DamageComponent
+    {
+        [HarmonyAfter("BEX.BattleTech.Extended_CE")]
+        public static bool Prefix(MechComponent __instance, ComponentDamageLevel damageLevel)
+        {
+            if (__instance.DamageLevel == damageLevel || __instance.DamageLevel == ComponentDamageLevel.Destroyed)
+                return false;
+            return true;
+        }
+    }
+}
