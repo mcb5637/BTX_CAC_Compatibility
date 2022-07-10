7e122d99ed0d910072e541a908b41ecfc8c88824
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2022-07-10 13:31:17 +0200

    version bump

--- BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/Main.cs
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/Main.cs
@@ -13,7 +13,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 
-[assembly: AssemblyVersion("0.1.19.4")]
+[assembly: AssemblyVersion("0.1.19.5")]
 
 namespace BTX_CAC_CompatibilityDll
 {
