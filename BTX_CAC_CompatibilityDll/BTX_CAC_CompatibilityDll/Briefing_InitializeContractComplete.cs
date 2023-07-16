88cac35b17461beb4145aea993d5d91b31027515
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2023-07-16 14:07:12 +0200

    infinite loading screen fix

---
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/Briefing_InitializeContractComplete.cs
@@ -0,0 +1,18 @@
+﻿using BattleTech;
+using BattleTech.UI;
+using Harmony;
+
+namespace BTX_CAC_CompatibilityDll
+{
+    [HarmonyPatch(typeof(Briefing), "InitializeContractComplete")]
+    public class Briefing_InitializeContractComplete
+    {
+        public static void Prefix()
+        {
+            var o = UnityGameInstance.BattleTechGame.SaveManager.GameInstanceSaves;
+            if (o.SaveFailedPopupVisible)
+                Traverse.Create(o).Property("SaveFailedPopupVisible").SetValue(false);
+        }
+    }
+
+}
