5b23e1f508c7d0d3dfe477732f8ed1c518e881da
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2024-03-10 14:58:09 +0100

    fix jamm chance calc

---
+++ BTX_CAC_CompatibilityDll/CAC_Calc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+
+
+CultureInfo i = new CultureInfo("en-us");
+CultureInfo.DefaultThreadCurrentCulture = i;
+CultureInfo.DefaultThreadCurrentUICulture = i;
+while (true)
+{
+    string? l = Console.ReadLine();
+    if (l == null || l == "" || l == "exit")
+        break;
+    if (l.StartsWith("jam"))
+    {
+        l = l[3..];
+        string[] a = l.Trim().Split('-');
+        if (a.Length != 2)
+        {
+            Console.WriteLine("error, missing min-max");
+            continue;
+        }
+        int b = 10;
+        double min = double.Parse(a[0]);
+        double max = double.Parse(a[1]);
+        Console.WriteLine($"\"FlatJammingChance\": {min},");
+        Console.WriteLine($"\"GunneryJammingBase\": {b},");
+        Console.WriteLine($"\"GunneryJammingMult\": {(max-min)/b},");
+    }
+}
