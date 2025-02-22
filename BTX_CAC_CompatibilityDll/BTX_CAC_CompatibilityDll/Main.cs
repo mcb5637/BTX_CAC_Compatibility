d9aa1e44a65a1510216b69c2b8781c5b3dea8dd2
mcb5637 <28106698+mcb5637@users.noreply.github.com>
2025-02-22 17:29:21 +0100

    fix drop slots

--- BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/Main.cs
+++ BTX_CAC_CompatibilityDll/BTX_CAC_CompatibilityDll/Main.cs
@@ -23,7 +23,7 @@ using System.Threading.Tasks;
 using UIWidgets;
 using UnityEngine;
 
-[assembly: AssemblyVersion("2.0.0.4")]
+[assembly: AssemblyVersion("2.0.1.0")]
 
 namespace BTX_CAC_CompatibilityDll
 {
